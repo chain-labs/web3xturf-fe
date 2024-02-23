58a3446165ce53d97add4ceb55a00473504a448f
Angel Lakra <angel.lakra@chainlabs.in>
2024-02-23 13:48:18 +0530

    update on seo and hd icons

--- src/copy.ts
+++ src/copy.ts
@@ -6,6 +6,15 @@ export const dateTimeText = "25th Feb 2024 | 8:00 AM - 10:00 AM IST";
 
 export const locationText = "LocoBear Turf, Koramangala | Bangalore";
 
+export const PAGE_TITLE = "Web3XTurf | Bangalore";
+
+export const SEO_DESCRIPTION = `Web3XTurf event being held at ${locationText} on ${dateTimeText}.`;
+
+export const SEO_KEYWORDS = "web3xturf, bangalore, web3, simplr, simplr-events";
+
+export const SEO_IMAGE =
+  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/Header%20Image_TeHVC4hMG.jpg?updatedAt=1708675987532";
+
 export const BG_GRADIENT =
   "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/Gradient_1TJhV4jEM.svg?updatedAt=1708624116193";
 
@@ -13,19 +22,19 @@ export const BG_ELEMENT =
   "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/bg-element_87LKC2mquK.png?updatedAt=1708661840242";
 
 export const LOGO =
-  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/logo_IEh4elh9dA.png?updatedAt=1708621635454";
+  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/logo_NO7ZVzc_m4.svg?updatedAt=1708675494382";
 
 export const CALENDAR_ICON =
-  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/calendar_icon_GGrgs_AAE.png?updatedAt=1708621635518";
+  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/calendar_icon_xh9-mLmhm.svg?updatedAt=1708675494387";
 
 export const LOCATION_ICON =
-  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/location_icon_k1tnqbbcP5.png?updatedAt=1708621635462";
+  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/location_icon_EaDeDPfGjN.svg?updatedAt=1708675494423";
 
 export const TICKET_ICON =
-  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/mdi_ticket_NY3P3yaJf.png?updatedAt=1708664706004";
+  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/mdi_ticket_XC6zkddoV.svg?updatedAt=1708675494271";
 
 export const ORGANIZER =
-  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/_z:0_world_qhXTYfr06G.png?updatedAt=1708621635457";
+  "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/zohouse_UXHI9mrrC.svg?updatedAt=1708675494365";
 
 export const SIMPLR_EVENTS =
   "https://ik.imagekit.io/chainlabs/Mint_Pages/web3xturf/simplr-events_ybNziFnmHf.svg?updatedAt=1708670026934";
